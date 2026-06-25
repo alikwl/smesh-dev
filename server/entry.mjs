ff0929cc0922a642c5ad833f08aabf34c34401e5
alikwl <alikwl@users.noreply.github.com>
2026-06-25 03:52:53 +0000

    deploy: d3e36488a75754c4e8fee2d84d8811c9a0c119fa

--- server/entry.mjs
+++ server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_aQcBT3Nh.mjs';
-import { manifest } from './manifest_CWEIA_gI.mjs';
+import { manifest } from './manifest_Yre7xrBe.mjs';
 
 const serverIslandMap = new Map();;
 
